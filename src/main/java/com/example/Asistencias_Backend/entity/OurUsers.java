2c8de4bd4d2981a2595bfb4a7ad701eae38af808
ItsGustavoSebas <gusta25demarzo@gmail.comgit config --global user.name Tu>
2024-06-01 22:53:26 -0400

    Seeders

--- src/main/java/com/example/Asistencias_Backend/entity/OurUsers.java
+++ src/main/java/com/example/Asistencias_Backend/entity/OurUsers.java
@@ -23,7 +23,7 @@ public class OurUsers implements UserDetails {
     private String email;
     private String name;
     private String password;
-    @ManyToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+    @ManyToMany
     @JoinTable(name = "user_roles", joinColumns = @JoinColumn(name = "user_id", referencedColumnName = "id"),
             inverseJoinColumns = @JoinColumn(name = "role_id", referencedColumnName = "id"))
     private List<Role> roles = new ArrayList<>();
